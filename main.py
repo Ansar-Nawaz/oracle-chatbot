188c571fe615106506c6912f29b47b90dde45bbe
orasniper <orasniper@localhost.localdomain>
2025-02-01 22:53:04 +0500

    fixup

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    import os
-    # Use the PORT from the environment or default to 5000 if not set.
+    # Retrieve the PORT environment variable set by Railway; default to 5000 if not set
     port = int(os.environ.get("PORT", 5000))
+    # Run the app, listening on all network interfaces (0.0.0.0) on the specified port
     app.run(host="0.0.0.0", port=port)
 
